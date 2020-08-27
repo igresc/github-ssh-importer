0ad81e226a9e5f818d16531354cbd313b5dd1d6a
igresc <sergicastro2001@gmail.com>
2020-08-27 20:12:53 +0000

    Correct usage and user confirmation

--- github-ssh-keys.sh
+++ github-ssh-keys.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-if [[ $# -eq 1 ]]; then
-    echo "Getting ssh keys from github for user $1"
-    # api_response=$(curl -s https://api.github.com/users/$1/keys)
+# Test if 
 
-    api_response=`cat json.json`
+if [[ -n "$1" ]]; then
+    # Asks if the user inserted is correct
+    read -p "Getting ssh keys from github for user $1 are you sure? [Y/n] " confirm
+    if [[ $confirm == n ]]
+    then
+        echo "Exit"
+        exit 0
+    fi
+    # api_response=$(curl -s https://api.github.com/users/$1/keys) # Get ssh keys for user from Github api
 
-    readarray -t array < <(sed -n "/{/,/}/{s/[^:]*:[[:blank:]]*//p}" <<<$api_response)
+    api_response=`cat json.json` # ONLY for testing Github api max requests
 
+    readarray -t array < <(sed -n "/{/,/}/{s/[^:]*:[[:blank:]]*//p}" <<<$api_response) # Convert json format into an string array
+
+    # From the array get the key and id, formatted to the correct form and added to the authoriezed_keys file.
     for i in "${!array[@]}"; do
         if [[ $((i % 2)) != 0 ]]; then
             echo "${array[i]} $1@github/${array[i - 1]} #github-key-importer" >>~/.ssh/authorized_keys
         fi
     done
 
-    sed -i -E 's/"|,//g' ~/.ssh/authorized_keys
+    sed -i -E 's/"|,//g' ~/.ssh/authorized_keys # Remove double quotes
+else
+    printf "illegal argument\n" >&2
+    echo " 
+Usage: 
+    ./github-ssh-keys.sh username
+    " >&2
 fi
